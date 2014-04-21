e2af5f7c67b2832a0bee6a7a6e77731fa0a19675
Mario Manno <mm@koeln.ccc.de>
2014-04-21 21:32:39 +0200

    remove attr_accessible

--- app/models/conference_export.rb
+++ app/models/conference_export.rb
@@ -1,6 +1,5 @@
 class ConferenceExport < ActiveRecord::Base
   belongs_to :conference
-  attr_accessible :locale, :tarball
   has_attached_file :tarball
   validates_attachment_content_type :tarball, content_type: [/gzip/]
   validates_presence_of :locale, :conference
