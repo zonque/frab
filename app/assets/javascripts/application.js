61cb118572f89e04fdd09813b71f4559e4881062
Mario Manno <mm@koeln.ccc.de>
2014-07-27 02:35:08 +0200

    remove bootstrap from application.js, fixes not found error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,14 +3,6 @@
 //= require twitter/bootstrap
 //= require jquery-ui
 //= require jquery-ui-timepicker-addon
-//= require bootstrap-alerts
-//= require bootstrap-dropdown
-//= require bootstrap-modal
-//= require bootstrap-twipsy
-//= require bootstrap-popover
-//= require bootstrap-scrollspy
-//= require bootstrap-tabs
-//= require bootstrap-buttons
 //= require cocoon
 //= require_tree .
 
